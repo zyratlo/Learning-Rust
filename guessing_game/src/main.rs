b1f1cb656a548b0cd94a0a9499c6757c4a6fee59
Ryan Zhang <zyratlo@gmail.com>
2023-12-03 15:32:04 -0800

    added comparing guess to secret number

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,10 +1,11 @@
 use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     println!("Guess the number!");
 
-    let secret_number: i32 = rand::thread_rng().gen_range(1..=100);
+    let secret_number: u32 = rand::thread_rng().gen_range(1..=100);
 
     println!("The secret number is: {secret_number}");
 
@@ -16,5 +17,13 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line");
 
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
     println!("You guessed: {guess}");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
