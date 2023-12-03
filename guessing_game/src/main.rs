1f5785fc37dcb1faa70f496f25e6c8e4cd75bfd9
Ryan Zhang <zyratlo@gmail.com>
2023-12-03 15:25:03 -0800

    added random secret number generation

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,11 +1,16 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
 
+    let secret_number: i32 = rand::thread_rng().gen_range(1..=100);
+
+    println!("The secret number is: {secret_number}");
+
     println!("Please input your guess.");
 
-    let mut guess = String::new();
+    let mut guess: String = String::new();
 
     io::stdin()
         .read_line(&mut guess)
